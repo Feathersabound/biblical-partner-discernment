6be62e468f2bdcd956cbf7fd190a3fcd881cd692
Feathersabound <239653751+Feathersabound@users.noreply.github.com>
2026-08-20 16:15:55 +0100

    Cache only the three app files

--- sw.js
+++ sw.js
@@ -1,36 +1,21 @@
-const CACHE = "walk-in-truth-v5";
-const FILES = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./data.js",
-  "./engine.js",
-  "./war-hook.js",
-  "./app.js",
-  "./manifest.json",
-  "./icon.svg"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+const CACHE = "walk-in-truth-v6";
+const FILES = ["./" ,"./index.html","./styles.css","./data.js","./engine.js","./app.js","./manifest.json","./icon.svg"];
+self.addEventListener("install", function (event) {
+  event.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(FILES); }));
   self.skipWaiting();
 });
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener("activate", function (event) {
+  event.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+  }));
   self.clients.claim();
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(hit => hit || fetch(event.request).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(c => c.put(event.request, copy));
+self.addEventListener("fetch", function (event) {
+  event.respondWith(caches.match(event.request).then(function (hit) {
+    return hit || fetch(event.request).then(function (res) {
+      var copy = res.clone();
+      caches.open(CACHE).then(function (c) { c.put(event.request, copy); });
       return res;
-    }).catch(() => caches.match("./index.html")))
-  );
+    }).catch(function () { return caches.match("./index.html"); });
+  }));
 });
