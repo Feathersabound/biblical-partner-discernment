b95dae50bd0d6e0485cb930e8050c9ae95b4344c
Feathersabound <239653751+Feathersabound@users.noreply.github.com>
2026-08-20 17:35:14 +0100

    Remove unused war-hook.js

--- war-hook.js
+++
@@ -1,7 +0,0 @@
-const _interactionPlan = interactionPlan;
-interactionPlan = function () {
-  const plan = _interactionPlan();
-  plan.extra = plan.extra || [];
-  plan.extra.push("This is a war. Jesus said we would have trouble. The thief comes to steal, kill, and destroy. Life is not always fair. Do not treat your spouse as the enemy. Jesus came that you may have life, and have it to the full.");
-  return plan;
-};
